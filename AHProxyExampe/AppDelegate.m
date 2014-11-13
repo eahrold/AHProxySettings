df4e66126532a88f455488b520714da120c9886c
Eldon Ahrold <eldon.ahrold@gmail.com>
2014-11-12 23:56:56 -0600

    Initial commit

---
+++ AHProxyExampe/AppDelegate.m
@@ -0,0 +1,102 @@
+//
+//  AppDelegate.m
+//  AHProxyExampe
+//
+//  Created by Eldon on 11/11/14.
+//  Copyright (c) 2014 Eldon Ahrold. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "AHProxySettings.h"
+#import "NSTask+useSystemProxies.h"
+
+@interface AppDelegate () {
+    AHProxySettings *_settings;
+    NSTask *_task;
+}
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+    _testURL.stringValue = @"https://github.com";
+    [self refresh:nil];
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+}
+
+- (IBAction)refresh:(id)sender {
+    _settings = [[AHProxySettings alloc] init];
+    _settings.useAutoDetectAsFailover = _useFailoverButton.state;
+    _settings.destinationURL = _testURL.stringValue;
+
+    _HTTPProxy.stringValue =
+        _settings.HTTPProxy ? _settings.HTTPProxy.exportString : @"";
+    _HTTPSProxy.stringValue =
+        _settings.HTTPSProxy ? _settings.HTTPSProxy.exportString : @"";
+    _FTPProxy.stringValue =
+        _settings.FTPProxy ? _settings.FTPProxy.exportString : @"";
+    _SOCKSProxy.stringValue =
+        _settings.SOCKSProxy ? _settings.SOCKSProxy.exportString : @"";
+    _AUTOProxy.stringValue =
+        _settings.autoDetectedProxies
+            ? [[_settings.autoDetectedProxies firstObject] exportString]
+            : @"";
+}
+
+- (IBAction)runTaskWithProxy:(NSButton *)sender {
+    [self setupTask];
+
+    NSMutableDictionary *env = [[NSMutableDictionary alloc] init];
+    [env addEntriesFromDictionary:[[NSProcessInfo processInfo]environment]];
+
+    [_task useSystemProxiesForDestination:_testURL.stringValue];
+
+    for( NSString *k in _task.environment) {
+        NSLog(@"%@",_task.environment[k]);
+    }
+
+    [_task launch];
+}
+
+- (IBAction)runTaskWithOutProxy:(id)sender {
+    [self setupTask];
+    [_task launch];
+}
+
+- (void)setupTask {
+    _runStatusTF.stringValue = @"Checking...";
+    [_runButton setEnabled:NO];
+    [_runWithoutButton setEnabled:NO];
+
+    _task = [[NSTask alloc] init];
+    _task.launchPath = @"/usr/bin/curl";
+    _task.arguments = @[ @"-k", _testURL.stringValue ];
+    _task.standardOutput = [NSPipe pipe];
+    _task.standardError = [NSPipe pipe];
+
+    __weak AppDelegate *weakSelf = self;
+    [_task setTerminationHandler:^(NSTask *task) {
+        if (task.terminationStatus > 0) {
+            NSData *data = [[task.standardError fileHandleForReading] readDataToEndOfFile];
+            NSString *errString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            NSLog(@"%@",errString);
+            weakSelf.runStatusTF.stringValue = [NSString stringWithFormat:@"Unable to connect to server [error code:%d]",task.terminationStatus ];
+        } else {
+            weakSelf.runStatusTF.stringValue = @"Successfully connected to server";
+        }
+        [weakSelf.runButton setEnabled:YES];
+        [weakSelf.runWithoutButton setEnabled:YES];
+    }];    
+}
+
+- (IBAction)cancelTask:(id)sender {
+    if (_task && _task.isRunning) {
+        [_task terminate];
+    }
+}
+@end
